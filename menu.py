15b50b685ccd91f9b0cd94fa82a7e13b206cdbbb
Gennady <galbert9@gmail.com>
2023-01-03 13:59:08 +0300

    Меню2

---
+++ menu.py
@@ -0,0 +1,7 @@
+from function import init_dict, write_new_contact_str, write_new_contact_list
+from user import search_ru
+
+def print_hint_ru():
+    print(''' \n Команды для работы со справочником''')
+
+#def menu():
